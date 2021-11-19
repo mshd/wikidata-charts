762b3cc620fbd82682a00e29c1e4853b1679c30a
Martin <mshd94@gmail.com>
2021-11-19 23:47:40 +0100

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_TRACKING_CODE || "placeholder", {
+  testMode: !process.env.NEXT_PUBLIC_GA_TRACKING_CODE,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
